e2b0590e67b57fbb90185af4df42fe0d262accb2
Homero Pawlowski <homeski2@gmail.com>
2017-07-26 00:18:47 -0400

    added influxdb functionality to server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var yaml = require('js-yaml')
 
 // Init
 const DEBUG = false;
+const DB_HOST = process.env.DB_NAME;
 const PORT = 8080;
 var BOT, TOKEN;
 
@@ -42,13 +43,20 @@ app.post('/groupme', function (req, res) {
   // Log incoming request body from chat post
   console.log('body: ' + JSON.stringify(req.body));
 
-  // clean the incoming message stringify
+  // Log every message into InfluxDB
+  var sender_id = req.body['sender_id'];
+  var sender_name = req.body['name'];
+  var cmd = "curl -i -XPOST 'http://" + DB_HOST + ":8086/write?db=mydb' --data-binary 'post,sender_id=" + sender_id + ",name=" + sender_name + " value=1 `date +'%s'`000000000";
+  shell.exec(cmd).stdout;
+
+  // Clean the incoming message stringify
   var text = req.body['text'].trim().toUpperCase();
 
   // Match any string containing 'macbot'
   if (text.indexOf('MACBOT') !== -1) {
     // Submit photo to groupme photo service and get the image URL back
-    var response = shell.exec("curl -s 'https://image.groupme.com/pictures' -X POST -H 'X-Access-Token: " + TOKEN + "' -H 'Content-Type: image/jpeg' --data-binary @./photos/`ls photos | shuf -n 1`").stdout;
+    var cmd = "curl -s 'https://image.groupme.com/pictures' -X POST -H 'X-Access-Token: " + TOKEN + "' -H 'Content-Type: image/jpeg' --data-binary @./photos/`ls photos | shuf -n 1`";
+    var response = shell.exec(cmd).stdout;
     var img_url = JSON.parse(response).payload.url;
     console.log('img_url: ' + img_url);
 
